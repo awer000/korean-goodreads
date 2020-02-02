6549caef29152cd8d82dba064a851f504c1bdf18
awer000 <awer000@naver.com>
2020-02-02 21:30:56 +0900

    #Day 2 - 책 목록 api 대신할 mock 데이터

---
+++ mock/swiperMock.ts
@@ -0,0 +1,17 @@
+export const imageMapList: object[] = [
+  {
+    src: "https://image.aladin.co.kr/product/22875/94/cover/k622636844_1.jpg"
+  },
+  {
+    src: "https://image.aladin.co.kr/product/22884/32/cover/895467030x_1.jpg"
+  },
+  {
+    src: "https://image.aladin.co.kr/product/22805/70/cover/k752636531_1.jpg"
+  },
+  {
+    src: "https://image.aladin.co.kr/product/22804/39/cover/k692636434_1.jpg"
+  },
+  {
+    src: "http://image.aladin.co.kr/product/22462/97/letslook/K482636526_f.jpg"
+  },
+]
