7d879910a770f57454d8c623d0e4a0b3de7868e1
awer000 <awer000@naver.com>
2020-02-03 12:42:41 +0900

    리스트 명칭 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import DefaultSwiper from "../components/DefaultSwiper";
 
 const IndexPage: NextPage = () => {
   return (
-    <>
-      <DefaultSwiper title={"이달의 신간"} />
+    <div className="Index">
+      <DefaultSwiper title={"주목할만한 신간"} />
       <DefaultSwiper title={"베스트 셀러"} />
       <DefaultSwiper title={"편집자 추천 리스트"} />
-    </>
+    </div>
   );
 };
 
