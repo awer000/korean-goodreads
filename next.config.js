1aaf3282bdbec0208910aef560ea8434465f31f4
awer000 <awer000@naver.com>
2020-02-06 15:55:58 +0900

    #Day 6 - next-bundle-analyzer config 설정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
-import withSass from '@zeit/next-sass';
-import withCss from '@zeit/next-css';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withSass = require('@zeit/next-sass');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withCss = require('@zeit/next-css');
 
-export default withCss(
-  withSass({
-    webpack(config, options) {
-      return config;
-    },
-  }),
+module.exports = withBundleAnalyzer(
+  withCss(
+    withSass({
+      distDir: '.next',
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../../bundles/server.html',
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/client.html',
+        },
+      },
+      cssModules: true,
+      webpack(config) {
+        return {
+          ...config,
+          mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+          devtool: process.env.NODE_ENV === 'production' ? 'hidden-source-map' : 'eval',
+        };
+      },
+    }),
+  ),
 );
