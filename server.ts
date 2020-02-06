018eed0a66fb410c083e6108698dc4b5833a5f40
awer000 <awer000@naver.com>
2020-02-06 14:22:20 +0900

    #Day 6 - 서버 index 파일 제작

---
+++ server.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import next from 'next';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
+import dotenv from 'dotenv';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+dotenv.config();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(morgan('dev'));
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  server.use(cookieParser(process.env.COOKIE_SECRET));
+  server.use(
+    expressSession({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET as string,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+      },
+    }),
+  );
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+  server.listen(3000, () => {
+    console.log('running on port 3000');
+  });
+});
+
+export default undefined;
